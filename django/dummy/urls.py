5fa424afca70c75e2052d387d800c8dea1dceaed
zimamdero <80876363+zimamdero@users.noreply.github.com>
2021-03-29 12:44:19 +0900

    public hello added

--- django/dummy/urls.py
+++ django/dummy/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from dummy.views import HelloView
+from dummy.views import HelloView, PublicHelloView
 
 
 urlpatterns = [
     path('hello/', HelloView.as_view(), name='hello'),
+    path('hello/public/', PublicHelloView.as_view(), name='hello_public'),
 ]
--- django/dummy/views.py
+++ django/dummy/views.py
@@ -1,8 +1,18 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 
 class HelloView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        content = {'message': 'Hello, World!'}
+        return Response(content)
+
+
+class PublicHelloView(APIView):
+    permission_classes = (AllowAny,)
 
     def get(self, request):
         content = {'message': 'Hello, World!'}
